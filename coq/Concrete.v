c7c60736931d6d1a703a71cc04c1b1598b28d145
David Trabish <davidtr1037@gmail.com>
2024-08-01 09:36:05 +0300

    updating

--- coq/Concrete.v
+++ coq/Concrete.v
@@ -50,7 +50,48 @@ Record state : Type := mk_state {
   module : llvm_module;
 }.
 
+Definition assert_id := (Name "__assert_fail").
+Definition assert_exp : exp typ := (EXP_Ident (ID_Global assert_id)).
+Definition assert_type :=
+  TYPE_Function
+    TYPE_Void
+    [
+      (TYPE_Pointer (TYPE_I 8));
+      (TYPE_Pointer (TYPE_I 8));
+      (TYPE_I 32);
+      (TYPE_Pointer (TYPE_I 8))
+    ]
+    false
+.
+
+(* TODO: move to LLVMAst/CFG? *)
+Fixpoint get_arg_types (args : list (function_arg)) : list typ :=
+  match args with
+  | ((t, e), _) :: tail => t :: get_arg_types tail
+  | [] => []
+  end
+.
+
 Inductive error_state : state -> Prop :=
+  | ES_Assert : forall ic cid args anns cs pbid ls stk gs m d,
+      (find_function m assert_id) = None ->
+      (find_declaration m assert_id) = Some d ->
+      (dc_type d) = assert_type ->
+      TYPE_Function TYPE_Void (get_arg_types args) false = assert_type ->
+      error_state
+        (mk_state
+          ic
+          (CMD_Inst
+            cid
+            (INSTR_VoidCall (TYPE_Void, assert_exp) args anns)
+          )
+          cs
+          pbid
+          ls
+          stk
+          gs
+          m
+       )
 .
 
 Definition build_inst_counter (m : llvm_module) (d : llvm_definition) : option inst_counter :=
@@ -212,13 +253,6 @@ Definition find_function_by_exp (m : llvm_module) (e : exp typ) : option llvm_de
   end
 .
 
-Fixpoint get_arg_types (args : list (function_arg)) : list typ :=
-  match args with
-  | ((t, e), _) :: tail => t :: get_arg_types tail
-  | [] => []
-  end
-.
-
 Definition eval_arg (ls : dv_store) (gs : global_store) (arg : function_arg) : option dynamic_value :=
   match arg with
   | ((t, e), _) => (eval_exp ls gs (Some t) e)
@@ -280,14 +314,14 @@ Definition get_trailing_cmds (d : llvm_definition) (ic : inst_counter) : option 
   end
 .
 
-Definition special_functions : list function_id := [
-  (Name "__assert_fail")
-].
+Definition klee_make_symbolic_int32_id := (Name "klee_make_symbolic_int32").
+Definition klee_make_symbolic_int32_exp : exp typ :=
+  EXP_Ident (ID_Global klee_make_symbolic_int32_id).
+Definition klee_make_symbolic_int32_type := TYPE_Function (TYPE_I 32) [] false.
 
-Definition klee_make_symbolic_int32 : exp typ :=
-  EXP_Ident (ID_Global (Name "klee_make_symbolic_int32")).
-Definition klee_assume : exp typ :=
-  EXP_Ident (ID_Global (Name "klee_assume")).
+Definition klee_assume_id := (Name "klee_assume").
+Definition klee_assume_exp : exp typ := EXP_Ident (ID_Global klee_assume_id).
+Definition klee_assume_type := TYPE_Function TYPE_Void [(TYPE_I 64)] false.
 
 Inductive step : state -> state -> Prop :=
   | Step_OP : forall ic cid v e c cs pbid ls stk gs m dv,
@@ -415,7 +449,6 @@ Inductive step : state -> state -> Prop :=
         )
   | Step_VoidCall : forall ic cid f args anns c cs pbid ls stk gs m d b c' cs' dvs ls',
       (find_function_by_exp m f) = Some d ->
-      ~ (In (dc_name (df_prototype d))  special_functions) ->
       (dc_type (df_prototype d)) = TYPE_Function TYPE_Void (get_arg_types args) false ->
       (entry_block d) = Some b ->
       (blk_cmds b) = c' :: cs' ->
@@ -444,7 +477,6 @@ Inductive step : state -> state -> Prop :=
         )
   | Step_Call : forall ic cid v t f args anns c cs pbid ls stk gs m d b c' cs' dvs ls',
       (find_function_by_exp m f) = Some d ->
-      ~ (In (dc_name (df_prototype d))  special_functions) ->
       (dc_type (df_prototype d)) = TYPE_Function t (get_arg_types args) false ->
       (entry_block d) = Some b ->
       (blk_cmds b) = c' :: cs' ->
@@ -522,12 +554,14 @@ Inductive step : state -> state -> Prop :=
           gs
           m
         )
-  (* TODO: check the actual type of klee_make_symbolic_int32? *)
-  | Step_MakeSymbolicInt32 : forall ic cid v c cs pbid ls stk gs m n,
+  | Step_MakeSymbolicInt32 : forall ic cid v c cs pbid ls stk gs m n d,
+      (find_function m klee_make_symbolic_int32_id) = None ->
+      (find_declaration m klee_make_symbolic_int32_id) = Some d ->
+      (dc_type d) = klee_make_symbolic_int32_type ->
       step
         (mk_state
           ic
-          (CMD_Inst cid (INSTR_Call v ((TYPE_I 32), klee_make_symbolic_int32) [] []))
+          (CMD_Inst cid (INSTR_Call v ((TYPE_I 32), klee_make_symbolic_int32_exp) [] []))
           (c :: cs)
           pbid
           ls
@@ -546,14 +580,17 @@ Inductive step : state -> state -> Prop :=
           m
         )
   (* TODO: what is the expected type of the argument (t)? *)
-  (* TODO: check the actual type of klee_assume? *)
-  | Step_Assume : forall ic cid t e attrs c cs pbid ls stk gs m dv,
+  | Step_Assume : forall ic cid t e attrs c cs pbid ls stk gs m d dv,
+      (find_function m klee_assume_id) = None ->
+      (find_declaration m klee_assume_id) = Some d ->
+      (dc_type d) = klee_assume_type ->
       (eval_exp ls gs (Some t) e) = Some dv ->
+      (* TODO: verify this... *)
       (convert Trunc dv t (TYPE_I 1)) = Some (DV_I1 one) ->
       step
         (mk_state
           ic
-          (CMD_Inst cid (INSTR_VoidCall (TYPE_Void, klee_assume) [((t, e), attrs)] []))
+          (CMD_Inst cid (INSTR_VoidCall (TYPE_Void, klee_assume_exp) [((t, e), attrs)] []))
           (c :: cs)
           pbid
           ls
