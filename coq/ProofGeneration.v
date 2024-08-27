ef12856c6e1de60f8780de5bc3cdf348e10d0cc3
David Trabish <davidtr1037@gmail.com>
2024-08-27 20:59:07 +0300

    adding file

---
+++ coq/ProofGeneration.v
@@ -0,0 +1,54 @@
+From Coq Require Import List.
+
+Import ListNotations.
+
+From SE Require Import BitVectors.
+From SE Require Import CFG.
+From SE Require Import ChoiceAxiom.
+From SE Require Import Completeness.
+From SE Require Import Concrete.
+From SE Require Import DynamicValue.
+From SE Require Import ExecutionTreeOpt.
+From SE Require Import LLVMAst.
+From SE Require Import ModuleAssumptions.
+From SE Require Import Symbolic.
+From SE Require Import Relation.
+From SE Require Import WellDefinedness.
+
+From SE.SMT Require Import Expr.
+From SE.SMT Require Import Model.
+
+From SE.Utils Require Import IDMap.
+
+Lemma LAUX_not_error_instr_op : forall ic cid v e cs pbid ls stk gs syms pc mdl,
+  ~ error_sym_state
+    (mk_sym_state
+      ic
+      (CMD_Inst cid (INSTR_Op v e))
+      cs
+      pbid
+      ls
+      stk
+      gs
+      syms
+      pc
+      mdl
+    ).
+Proof.
+  intros ic cid v e cs pbid ls stk gs syms pc mdl.
+  intros H.
+  inversion H.
+Qed.
+
+
+Lemma LAUX_1 : forall s v se1 se2 se3,
+  Some se1 = Some se2 ->
+  equiv_smt_expr se1 se3 ->
+  equiv_smt_store (v !-> Some se2; s) (v !-> Some se3; s).
+Proof.
+  intros s v se1 se2 se3 H Heq.
+  inversion H; subst.
+  apply equiv_smt_store_update.
+  { apply equiv_smt_store_refl. }
+  { assumption. }
+Qed.
