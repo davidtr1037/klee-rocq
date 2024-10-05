4fd286e3cd7f59b6de7ddcfb37a8dddfb09beae0
David Trabish <davidtr1037@gmail.com>
2024-10-05 22:48:17 +0300

    updating test

--- coq/examples/test_12.c
+++ coq/examples/test_12.c
@@ -4,11 +4,25 @@
 
 #include <klee/klee.h>
 
-int main() {
+void f1() {
     unsigned x = 3;
     unsigned y = 2;
     unsigned n = klee_make_symbolic_int32();
-    unsigned a = x % y;
     unsigned b = n % x;
+}
+
+void f2(unsigned x, unsigned y) {
+    unsigned a = x % y;
+}
+
+void f3() {
+    unsigned n = klee_make_symbolic_int32();
+    unsigned a = n % 1;
+}
+
+int main() {
+    f1();
+    //f2(2, 3);
+    //f3();
     return 0;
 }
