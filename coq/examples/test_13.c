4baaff73fd215d9a8792ec7faeeca506bcfd60ca
David Trabish <davidtr1037@gmail.com>
2024-10-06 22:09:57 +0300

    adding file

---
+++ coq/examples/test_13.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <klee/klee.h>
+
+void f1() {
+    uint32_t x = 0;
+    uint32_t y = klee_make_symbolic_int32();
+    uint64_t a;
+    a = x;
+    a = y;
+}
+
+int main() {
+    f1();
+    return 0;
+}
