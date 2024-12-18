e7b61b2d7f0e84790aabae673f27dd5044808fdc
David Trabish <davidtr1037@gmail.com>
2024-12-18 22:24:23 +0200

    updating test

--- coq/examples/test_20.c
+++ coq/examples/test_20.c
@@ -14,6 +14,16 @@ void test_udiv_2() {
     unsigned y = x / 2;
 }
 
+void test_udiv_3(unsigned x) {
+    unsigned y = 2 / x;
+}
+
+void test_udiv_4() {
+    unsigned x = klee_make_symbolic_int32();
+    klee_assume_bool(x > 0);
+    unsigned y = 2 / x;
+}
+
 void test_sdiv_1(int x) {
     int y = x / 2;
 }
@@ -23,10 +33,24 @@ void test_sdiv_2() {
     int y = x / 2;
 }
 
+void test_sdiv_3(unsigned x) {
+    int y = 2 / x;
+}
+
+void test_sdiv_4() {
+    int x = klee_make_symbolic_int32();
+    klee_assume_bool(x > 0);
+    int y = 2 / x;
+}
+
 int main() {
     test_udiv_1(100);
     test_udiv_2();
+    test_udiv_3(100);
+    test_udiv_4();
     test_sdiv_1(100);
     test_sdiv_2();
+    test_sdiv_3(100);
+    test_sdiv_4();
     return 0;
 }
