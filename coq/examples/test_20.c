bb95b1b272f3b698df9058fc42ab89c8e6d401f9
David Trabish <davidtr1037@gmail.com>
2025-01-09 13:14:28 +0200

    updating

--- coq/examples/test_20.c
+++ coq/examples/test_20.c
@@ -43,14 +43,48 @@ void test_sdiv_4() {
     int y = 2 / x;
 }
 
+void test_sdiv_5() {
+    int x = klee_make_symbolic_int32();
+    klee_assume_bool(x != 0);
+    int y = 100 / x;
+}
+
+void test_sdiv_6() {
+    int x = klee_make_symbolic_int32();
+    klee_assume_bool(x != 0);
+    klee_assume_bool(x != -1);
+    int y = INT32_MIN / x;
+}
+
+void test_sdiv_7() {
+    int x = klee_make_symbolic_int32();
+    klee_assume_bool(x != INT32_MIN);
+    int y = x / -1;
+}
+
+void test_sdiv_8() {
+    int x = klee_make_symbolic_int32();
+    int y = klee_make_symbolic_int32();
+    klee_assume_bool(x != INT32_MIN);
+    klee_assume_bool(y != 0);
+    klee_assume_bool(y != -1);
+    int z = x / y;
+}
+
 int main() {
     test_udiv_1(100);
     test_udiv_2();
     test_udiv_3(100);
     test_udiv_4();
+
     test_sdiv_1(100);
     test_sdiv_2();
     test_sdiv_3(100);
     test_sdiv_4();
+    test_sdiv_5();
+    test_sdiv_6();
+    test_sdiv_7();
+    test_sdiv_8();
+
     return 0;
 }
